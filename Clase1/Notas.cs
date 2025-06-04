78dce08c0b5ce0bcd33213d9e4195dc755551e51
UCLAB220 <UCLAB220@UCLAB220>
2025-06-04 17:40:33 -0500

    Agregar archivos de proyecto.

---
+++ Clase1/Notas.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    public class Notas
+    {
+        int[] notaT1 = new int[20];
+        int[] notaT2 = new int[20];
+        int[] notaT3 = new int[20];
+        int[] notaEF = new int[20];
+        public void CalificarT1()
+        {
+            Random r = new Random();
+            for (int i = 0; i < notaT1.Length; i++)
+            {
+                notaT1[i] = r.Next(1, 21);
+            }
+        }
+        private void MostrarT1()
+        {
+            foreach (int item in notaT1)
+            {
+                Console.WriteLine(item);
+            }
+            //PARA CADA x PONLE UN y, 
+            //Usamos ForEach, pero tambíés es posible usar For
+            //A continuación un ejemplo de como sería:
+
+            /*for (int i = 0; i < notaT1.Length; i++)
+            {
+                Console.WriteLine(notaT1[i]);
+            }
+            */
+        }
+        public void CalificarT2()
+        {
+            Random r = new Random();
+            for (int i = 0; i < notaT1.Length; i++)
+            {
+                notaT2[i] = r.Next(1, 21);
+            }
+        }
+        private void MostrarT2()
+        {
+            foreach (int item in notaT2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void CalificarT3()
+        {
+            Random r = new Random();
+            for (int i = 0; i < notaT1.Length; i++)
+            {
+                notaT3[i] = r.Next(1, 21);
+            }
+        }
+        private void MostrarT3()
+        {
+            foreach (int item in notaT3)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void CalificarEF()
+        {
+            Random r = new Random();
+            for (int i = 0; i < notaT1.Length; i++)
+            {
+                notaEF[i] = r.Next(1, 21);
+            }
+        }
+        private void MostrarEF()
+        {
+            foreach (int item in notaEF)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void MostrarNotas(int op)
+        {
+            switch (op)
+            {
+                case 0:
+                    Console.WriteLine("Saliendo...");
+                    break;
+                case 1:
+                    MostrarT1();
+                    break;
+                case 2:
+                    MostrarT2();
+                    break;
+                case 3:
+                    MostrarT3();
+                    break;
+                case 4:
+                    MostrarEF();
+                    break;
+                case 5:
+                    for (int i = 0; i < notaT1.Length; i++)
+                    {
+                        double prom = ((double)notaT1[i] + notaT2[i] + notaT3[i] + notaEF[i])/4;
+                        Console.WriteLine("Alumno " + i + ": " + notaT1[i]
+                            + "\t" + notaT2[i]
+                            + "\t" + notaT3[i]
+                            + "\t" + notaEF[i]
+                             "\t" + prom);
+
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Ingrese una opción valida");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Semana11_FunAlg_13126/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Clase1;
+
+namespace Semana11_FunAlg_13126
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notas FunAlg_13126 = new Notas();
+
+            FunAlg_13126.CalificarT1();
+            Thread.Sleep(2);
+            FunAlg_13126.CalificarT2();
+            Thread.Sleep(2);
+            FunAlg_13126.CalificarT3();
+            Thread.Sleep(2);
+            FunAlg_13126.CalificarEF();
+
+            int op;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(":!:! NOTAS DE FUN. ALGORITMOS :!:! ");
+                Console.WriteLine("1. Mostrar Notas T1");
+                Console.WriteLine("2. Mostrar Notas T2");
+                Console.WriteLine("3. Mostrar Notas T3");
+                Console.WriteLine("4. Mostrar Notas EF");
+                Console.WriteLine("0. Salir");
+                Console.WriteLine("Ingrese una opcion...");
+                op = int.Parse(Console.ReadLine());
+
+                FunAlg_13126.MostrarNotas(op);
+                Console.ReadKey();
+            }
+            while (op!=0);
+        }
+        
+    }
+}
